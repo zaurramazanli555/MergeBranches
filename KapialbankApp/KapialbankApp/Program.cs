85fc0fba8b5185810baf7e2ed09e0937c0846598
Zaur Ramazanli <zaurramazanli555@gmail.com>
2021-12-09 21:39:09 +0300

    sadjsdas

---
+++ KapialbankApp/KapialbankApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapialbankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebApp();
+            MobileApp();
+            APISayt();
+        }
+        public static void WebApp()
+        {
+            Console.WriteLine("This is Web Application");
+        }
+        public static void MobileApp()
+        {
+            Console.WriteLine("This is Mobile  Application");
+        }
+        public static void APISayt()
+        {
+            Console.WriteLine("This is Api");
+        }
+    }
+}
